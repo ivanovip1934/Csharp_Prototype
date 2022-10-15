662176793c5e602c3af27994ef417ccc7dde6afa
ivanovip1934 <ivanovip1934@outlook.com>
2022-10-15 18:30:04 +0300

    Add project files.

---
+++ Csharp_Prototype/EmployeeFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Prototype
+{
+
+    [Serializable]
+    public class Contact
+    {
+        public string Name;
+        public Address WorkAddress;
+    }
+    [Serializable]
+    public class Address
+    {
+        public string Street;
+        public string City;
+        public int Suite;
+    }
+    internal class EmployeeFactory
+    {
+        private static Contact mainoffice = new Contact
+            {
+                Name = "John",
+                WorkAddress = new Address { City = "London", Street = "221B Baker St", Suite = 100 }
+            };
+        private static Contact liverpul = new Contact
+        {
+            Name = "John",
+            WorkAddress = new Address { City = "Liverpul", Street = "221B Baker St", Suite = 100 }
+        };
+        static Contact NewEmployee(Contact prototype,string Name, int Suite)
+        {
+            var result = prototype.DeepCopy();
+            result.Name = Name;
+            result.WorkAddress.Suite = Suite;
+            return result;
+        }
+
+        public static Contact NewMainOfficeEmployee(string Name,int Suite) 
+        { 
+            return NewEmployee(mainoffice, Name,Suite);
+        }
+        public static Contact NewLiverpulEmployee(string Name, int Suite)
+        {
+            return NewEmployee(liverpul, Name, Suite);
+        }
+    }
+}
---
+++ Csharp_Prototype/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Prototype
+{
+    internal class Program
+    {       
+
+        static void Main(string[] args)
+        {
+            var john = EmployeeFactory.NewMainOfficeEmployee("John", 100);
+            var den = EmployeeFactory.NewLiverpulEmployee("Den", 147);
+
+            Console.WriteLine($@"{john.Name} {john.WorkAddress.City}");
+            Console.WriteLine($@"{den.Name} {den.WorkAddress.City}");
+            Console.ReadKey();  
+        }
+    }
+}
